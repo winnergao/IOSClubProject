66fbeadb07bc5e81b92d9ba6b0ca4f0880f9d09d
竞华 高 <jinghua@jinghuatekimacbook-pro.local>
2016-02-22 19:27:10 -0500

    balance files created

---
+++ IOSClubProject/BalanceViewController.h
@@ -0,0 +1,13 @@
+//
+//  BalanceViewController.h
+//  IOSClubProject
+//
+//  Created by 竞华 高 on 16/2/22.
+//  Copyright © 2016年 winnergao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BalanceViewController : UIViewController
+
+@end
---
+++ IOSClubProject/BalanceViewController.m
@@ -0,0 +1,27 @@
+//
+//  BalanceViewController.m
+//  IOSClubProject
+//
+//  Created by 竞华 高 on 16/2/22.
+//  Copyright © 2016年 winnergao. All rights reserved.
+//
+
+#import "BalanceViewController.h"
+
+@interface BalanceViewController ()
+
+@property(nonatomic, weak) IBOutlet UIImageView *leftArrowImageView;
+@property(nonatomic, weak) IBOutlet UIImageView *rightAroowImageView;
+
+@end
+
+@implementation BalanceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
